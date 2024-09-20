89f8358224966a3e433bf76346e6a5eae7125c71
kkeksbtw <154913651+kkeksbtw@users.noreply.github.com>
2024-09-20 11:59:07 -0400

    changes for class
    added two properties and visualized my card

--- src/my-card.js
+++ src/my-card.js
@@ -22,16 +22,85 @@ export class MyCard extends LitElement {
       :host {
         display: block;
       }
+
+      .card {
+  width: 300px;
+  padding: 20px;
+  text-align: center;
+  margin: 10px;
+  margin-left: auto;
+  margin-right: auto;
+  background-color: grey;
+  border: 12px solid;
+  border-radius: 10px;
+}
+
+.card img{
+  width: 100%;
+  max-width: 500px;
+  display: block;
+  margin: auto;
+  border: 1px solid;
+  border-radius: 1px;
+}
+
+.details-bt{
+  background-color: skyblue;
+  color: black;
+  padding: 8px;
+  font-size: 20px;
+  border-radius: 10px;
+}
+.deatils-bt:hover{
+  background-color: darkblue;
+  color: white;
+}
+
+@media(min-width: 501px) and (max-width: 799px) 
+{
+  .details-bt{
+    
+  }
+}
+
+@media(max-width: 499px)
+{
+  .card img{
+    
+  } 
+}
     `;
   }
 
   render() {
-    return html`<div>${this.title}</div>`;
+    return html`
+
+    <div class="card">
+    <img src="https://images.wired.it/wp-content/uploads/2016/04/1461243989_mrrobot11.jpg" alt="The Day Crowdstrike Went Down">
+  
+    <h2> The Day Crowdstrike Went Down </h2>
+  
+    <p>  In this video we will explore how the lives of those working on crowdstrike during its crash have been forever altered </p>
+    <a href="https://hax.psu.edu" class="details" >
+    <div>
+      <button class="details-bt"> 
+      
+        WATCH 
+
+      </button>
+
+    </div>
+  </div>
+`;
+  
   }
 
   static get properties() {
+
     return {
       title: { type: String },
+      ImageSrc: { tpye: String },
+      p : { type: String }
     };
   }
 }
